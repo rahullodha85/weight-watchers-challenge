0f424a61badae9462d60b55549361eae5799dadd
Rahul Lodha <rahul_lodha@s5a.com>
2017-07-15 17:00:12 -0400

    question 2 solution

---
+++ src/main/java/exercise1/WebPageTest.java
@@ -0,0 +1,74 @@
+package exercise1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class WebPageTest {
+
+    static WebDriver driver;
+
+    public WebPageTest() {
+        getChromeDriver();
+        driver.navigate().to("file:///Users/461967/dev/weight-watchers-challenge/src/main/resources/testPage.html");
+    }
+
+    public WebDriver getChromeDriver() {
+        String driverPath = System.getProperty("user.dir") + "/drivers/chromedriver";
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver(getChromeOptions());
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        return driver;
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-geolocation");
+
+        return options;
+    }
+
+    public String readItemByIndex(int index) {
+        String item = driver.findElement(By.className("”itemContent”")).findElements(By.tagName("span")).get(index).getText();
+        return item;
+    }
+
+    public Map<String, String> readAll() {
+        Map<String, String> map = new HashMap<>();
+        List<WebElement> foodItems = driver.findElement(By.className("”itemContent”"))
+                .findElements(By.cssSelector("span[ng-bind='food.name']"));
+        List<WebElement> foodServing = driver.findElement(By.className("”itemContent”"))
+                .findElements(By.cssSelector("span[ng-bind='food._servingDesc']"));
+
+        for (int i = 0; i < foodItems.size(); i++) {
+            map.put(foodItems.get(i).getText(), foodServing.get(i).getText());
+        }
+        return map;
+    }
+
+    private void closeBrowser() {
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        WebPageTest test = new WebPageTest();
+        System.out.println("Third item from list: " + test.readItemByIndex(2));
+        System.out.println("Fifth item from list: " + test.readItemByIndex(4));
+        Map<String, String> items = test.readAll();
+        System.out.println("\nAll Items:");
+        for (String key : items.keySet()) {
+            System.out.println("Food Item: " + key + ", Serving: " + items.get(key));
+        }
+        test.closeBrowser();
+    }
+}
