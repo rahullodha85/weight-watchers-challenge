283208adf9520855cf471fa298d0927f8e4248c1
Rahul Lodha <rahul_lodha@s5a.com>
2017-07-16 20:37:19 -0400

    checking if file path exist

--- src/main/java/exercise1/FileParser.java
+++ src/main/java/exercise1/FileParser.java
@@ -1,6 +1,7 @@
 package exercise1;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
 public class FileParser {
@@ -11,27 +12,33 @@ public class FileParser {
         this.path = path;
     }
 
-    public boolean fileExists(String path) {
-        return false;
+    private boolean doesFileExists(String path) {
+        File f = new File(path);
+        if (f.exists())
+            return true;
+        else return false;
     }
 
     public void read() {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(path));
 
-            String sCurrentLine;
+        if (doesFileExists(path)) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(path));
 
-            while ((sCurrentLine = br.readLine()) != null) {
-                if (sCurrentLine.contains("-")) {
-                    String[] strArr = sCurrentLine.split("-|,");
-                    for (String str : strArr) {
-                        System.out.println(str.trim());
+                String sCurrentLine;
+
+                while ((sCurrentLine = br.readLine()) != null) {
+                    if (sCurrentLine.contains("-")) {
+                        String[] strArr = sCurrentLine.split("-|,");
+                        for (String str : strArr) {
+                            System.out.println(str.trim());
+                        }
+                        System.out.println();
                     }
-                    System.out.println();
                 }
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
             }
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
         }
     }
 }
